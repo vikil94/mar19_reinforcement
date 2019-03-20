0b2ad70adeb269dcda2ae7d5ee8db152ab5b7f22
Vikil Naik <vikilnaik37@gmail.com>
2019-03-20 11:02:33 -0400

    finished excercise

---
+++ excercise.py
@@ -0,0 +1,41 @@
+project = { 
+  'committee': ["Stella", "Salma", "Kai"],
+  'title': "Very Important Project",
+  'due_date': "December 14, 2019",
+  'id': "3284",
+  'steps': [
+    {'description': "conduct interviews",
+     'due_date': "August 1, 2018"
+    },
+    {'description': "code of conduct",
+     'due_date': "January 1, 2018"
+    },
+    {'description': "compile results",
+     'due_date': "November 10, 2018"
+    },
+    {'description': "version 1",
+     'due_date': "January 15, 2019"
+    },
+    {'description': "revisions",
+     'due_date': "March 30, 2019"
+    },
+    {'description': "version 2",
+     'due_date': "July 12, 2019"
+    },
+    {'description': "final edit",
+     'due_date': "October 1, 2019"
+    },
+    {'description': "final version",
+     'due_date': "November 20, 2019"
+    },
+    {'description': "Wrap up",
+     'due_date': "December 1, 2019"
+    }
+  ]
+}
+
+
+for i in range(0, len(project['steps'])):
+    project['steps'][i]['staff'] = project['committee'][i % 3]
+
+print(project)
